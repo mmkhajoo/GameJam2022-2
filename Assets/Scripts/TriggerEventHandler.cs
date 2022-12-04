3c8a6ded4ebb2b1004b576cac933d63602abc07b
mmkhajoo <mmkhajoo>
2022-12-04 21:07:35 +0330

    Level 1

---
+++ Assets/Scripts/TriggerEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+    public class TriggerEventHandler : MonoBehaviour
+    {
+        public UnityEvent OnTriggerActivate;
+
+        private void OnTriggerEnter2D(Collider2D collider2D)
+        {
+            if (collider2D.tag.Equals("Player"))
+            {
+                OnTriggerActivate?.Invoke();
+            }
+        }
+    }
+}
